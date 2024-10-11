bc823cfd8fcbf0486ea70b6caa54ae51bf808a25
Athallah <mathallahr37@gmail.com>
2024-10-11 22:12:24 +0700

    Update login.php

--- php/teslogin/login.php
+++ php/teslogin/login.php
@@ -16,15 +16,31 @@
     if(isset($_POST['login'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
+        $sql= "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $result = $db->query($sql);
+        if($result->num_rows > 0){
+            $data = $result->fetch_assoc();
+            header("location: dashboard.php");
+        }
+        else{
+            echo "<br>";
+            echo "akun tidak di temukan";
+            echo "<br>";
+            echo "atau";
+            echo "<br>";
+            echo "password salah";
+        }
     }
     ?>
     <h1>LOGINKAN</h1>
     <form action="login.php" method="post">
         <input type="text" name="username" placeholder="namamu">
         <br>
-        <input type="password" name="passwrod" placeholder="password">
+        <input type="password" name="password" placeholder="password">
         <br>
         <input type="submit" value="oke gas" name="login">
+        <?php 
+        ?>
     </form>
 </body>
 </html>
