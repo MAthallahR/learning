f271d9bec6b6b565e5c72026b4ee2403ee72078d
Athallah <mathallahr37@gmail.com>
2024-10-14 22:45:52 +0700

    Update login.php

--- php/teslogin/login.php
+++ php/teslogin/login.php
@@ -11,32 +11,35 @@
     }
 </style>
 <body>
-    <?php
-    include("service/database.php");
-    if(isset($_POST['login'])){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $sql= "SELECT * FROM users WHERE username='$username' AND password='$password'";
-        $result = $db->query($sql);
-        if($result->num_rows > 0){
-            $data = $result->fetch_assoc();
-            header("location: dashboard.php");
-        }
-        else{
-            echo "<br>";
-            echo "akun tidak di temukan";
-            echo "<br>";
-            echo "atau";
-            echo "<br>";
+<?php
+include("service/database.php");
+session_start();
+
+if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $sql= "SELECT * FROM users WHERE username='$username'";
+    $result = $db->query($sql);
+
+    if($result->num_rows > 0){
+        $data = $result->fetch_assoc();
+        if(password_verify($_POST['password'], $data["password"])){
+            $_SESSION["islogin"] = true;
+            $_SESSION["username"] = $data["username"];
+            header("Location: dashboard.php");
+        }else{
             echo "password salah";
         }
+    }else{
+        echo "akun tidak di temukan";
     }
-    ?>
+}
+?>
     <h1>LOGINKAN</h1>
     <form action="login.php" method="post">
-        <input type="text" name="username" placeholder="namamu">
+        <input type="text" name="username" placeholder="namamu" required>
         <br>
-        <input type="password" name="password" placeholder="password">
+        <input type="password" name="password" placeholder="password" required>
         <br>
         <input type="submit" value="oke gas" name="login">
         <?php 
