4a68a4220b3a75fecae146cd1ca5cfcbb8627c68
Athallah <mathallahr37@gmail.com>
2024-10-16 23:48:42 +0700

    Update register.php
    update css

--- php/teslogin/register.php
+++ php/teslogin/register.php
@@ -8,10 +8,65 @@
 <style>
     body{
         background-color: wheat;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+    }
+    form{
+        background-color: white;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 20px;
+        border-radius: 5px;
+        height: 350px;
+        width: 400px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    .usn, .pw{
+        margin-bottom: 10px;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+        font-size: 16px;
+        width: 100%;
+        box-sizing: border-box; 
+    }
+    .usn:hover, .pw:hover{
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    }
+    .usn:focus, .pw:focus{
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        font-size: larger;
+        transition: font-size .5s;
+    }
+    .register{
+        background-color: #4CAF50;
+        color: #fff;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+    .register:hover{
+        background-color: #3e8e41;
+        font-size: larger;
+        transition: font-size .5s;
     }
 </style>
 <body>
-<?php
+    <form action="register.php" method="post">
+        <h1>CEPATKAN DAFTAR</h1>
+        <input type="text" name="username" placeholder="namamu" required class="usn">
+        <br>
+        <input type="password" name="password" placeholder="password" class="pw">
+        <br>
+        <input type="submit" value="oke gas" name="register" class="register"> 
+        <br>
+        <p>sudah punya akun? <a href="login.php">LOGIN</a></p>
+        <?php
 include("service/database.php");
 if(isset($_POST['register'])){
     $username = $_POST['username'];
@@ -33,13 +88,6 @@ if(isset($_POST['register'])){
     }
 }
 ?>
-    <h1>CEPATKAN DAFTAR</h1>
-    <form action="register.php" method="post">
-        <input type="text" name="username" placeholder="namamu" required>
-        <br>
-        <input type="password" name="password" placeholder="password" required>
-        <br>
-        <input type="submit" value="oke gas" name="register">
     </form>
 </body>
 </html>
