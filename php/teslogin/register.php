a0feebc5c19c9d246adcdea88566e90769118d47
Athallah <mathallahr37@gmail.com>
2024-10-18 21:06:26 +0700

    Update register.php

--- php/teslogin/register.php
+++ php/teslogin/register.php
@@ -7,7 +7,7 @@
 </head>
 <style>
     body{
-        background-color: wheat;
+        background-color: #303030;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -21,11 +21,11 @@
         align-items: center;
         padding: 20px;
         border-radius: 5px;
-        height: 350px;
-        width: 400px;
+        height: 415px;
+        width: 450px;
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     }
-    .usn, .pw{
+    .usn, .pw, .email{
         margin-bottom: 10px;
         padding: 10px;
         border: none;
@@ -33,11 +33,12 @@
         font-size: 16px;
         width: 100%;
         box-sizing: border-box; 
+        border: 1px solid rgba(0, 0, 0, 0.233);
     }
-    .usn:hover, .pw:hover{
+    .usn:hover, .pw:hover, .email:hover{
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     }
-    .usn:focus, .pw:focus{
+    .usn:focus, .pw:focus, .email:focus{
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
         font-size: larger;
         transition: font-size .5s;
@@ -59,7 +60,9 @@
 <body>
     <form action="register.php" method="post">
         <h1>CEPATKAN DAFTAR</h1>
-        <input type="text" name="username" placeholder="namamu" required class="usn">
+        <input type="text" name="username" placeholder="nama" required class="usn">
+        <br>
+        <input type="email" name="email" placeholder="email" required class="email">
         <br>
         <input type="password" name="password" placeholder="password" class="pw">
         <br>
@@ -67,27 +70,42 @@
         <br>
         <p>sudah punya akun? <a href="login.php">LOGIN</a></p>
         <?php
-include("service/database.php");
-if(isset($_POST['register'])){
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        include("service/database.php");
+        if(isset($_POST['register'])){
+            $username = $_POST['username']; // Mengambil Username
+            $password = $_POST['password']; // Mengambil Password
+            $email = $_POST['email']; // Mengambil Email
 
-    $check_sql = "SELECT * FROM users WHERE username = '$username'";
-    $result = $db->query($check_sql);
+            // Mengecek apa password lebih dari 8 kata atau tidak
+            if (strlen($password) < 8) {
+                echo "password harus lebih dari 8 kata";
+            } else {
+                // Menyamarkan password jika sudah lebih dari 8 kata (kenapa harus disamarkan? karena function password_verify di login hanya bekerja jika di samarkan)
+                $hash_password = password_hash($password, PASSWORD_BCRYPT);
 
-    if($result->num_rows > 0){
-        echo "namamu sudah di pakai";
-    } else {
-        $sql = "INSERT INTO users (username,password) VALUES ('$username','$password')";
+                // Cek jika nama atau email sudah ada atau belum
+                $check_sql = "SELECT * FROM users WHERE username = '$username' OR email = '$email'";
+                $result = $db->query($check_sql);
 
-    if($db->query($sql)){
-            echo "register berhasil";
-    } else {
-            echo "register gagal";
-    }
-    }
-}
-?>
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    if ($row["username"] == $username) {
+                        echo "namamu sudah di pakai";
+                    } elseif ($row["email"] == $email) {
+                        echo "emailmu sudah di pakai";
+                    }
+                } else {
+                    // Memasukan data ke database
+                    $sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$hash_password', '$email')";
+                    if ($db->query($sql)) {
+                        echo "register berhasil";
+                    } else {
+                        echo "register gagal";
+                    }
+                }
+            }
+        }
+        ?>
     </form>
 </body>
 </html>
